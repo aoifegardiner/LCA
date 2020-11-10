ddb863e15d28d94f7086653755dba13ec44cf694
aoifegardiner <agardine@tcd.ie>
2020-11-10 16:57:46 +0000

    Empty test

--- LCA/src/DAGTest.java
+++ LCA/src/DAGTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -5,8 +6,9 @@ import org.junit.jupiter.api.Test;
 class DAGTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testLCAforEmpty() {
+		DAG lca = new DAG(10);
+		assertEquals("Testing LCA is -1", -1, lca.findLCA(1, 2));
 	}
 
 }
