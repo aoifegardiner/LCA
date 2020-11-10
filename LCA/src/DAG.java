457fbd7d9dc8ff1c4df41d8c09316642280fa177
aoifegardiner <agardine@tcd.ie>
2020-11-10 16:47:02 +0000

    Node and Variables

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
 
 public class DAG {
-	//test
+	
+	private int vertices;           	//Number of vertices in this digraph
+	private int edges;                 	//Number of edges in this digraph
+	private ArrayList<Integer>[] adj;   //adj[v] = adjacency list for vertex v
+	private int[] indegree;        		//Indegree[v] = indegree of vertex v
+	private boolean marked[];			//Boolean list to track visited vertices
+	private boolean stack[];			//Order that vertices were visited
+	private boolean hasCycle;			//True if cycle in graph
+	
+	@SuppressWarnings("unchecked")
+	public DAG(int vertices)
+	{
+		if (vertices < 0) throw new IllegalArgumentException("Must be a positive number of vertices.");
+	    this.vertices = vertices;
+	    this.edges = 0;
+	    indegree = new int[vertices];	
+	    marked = new boolean[vertices];
+	    stack = new boolean[vertices];
+	    adj = (ArrayList<Integer>[]) new ArrayList[vertices];
+	    for (int v = 0; v < vertices; v++) {
+	        adj[v] = new ArrayList<Integer>();
+	    }    
+	}
 }
+	
