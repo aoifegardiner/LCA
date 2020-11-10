2e1738f7c8dee2ac3704aba43b462f75652b93a7
aoifegardiner <agardine@tcd.ie>
2020-11-10 16:58:58 +0000

    Directed graph test

--- LCA/src/DAGTest.java
+++ LCA/src/DAGTest.java
@@ -10,5 +10,19 @@ class DAGTest {
 		DAG lca = new DAG(10);
 		assertEquals("Testing LCA is -1", -1, lca.findLCA(1, 2));
 	}
+	
+	@Test
+	public void testDirectedGraph(){
+		DAG newDAG = new DAG(10);
+		newDAG.addEdge(1, 2);
+		newDAG.addEdge(1, 3);
+		newDAG.addEdge(3, 4);
+		newDAG.addEdge(4, 5);
+		newDAG.addEdge(4, 6);
 
+		assertEquals("", 5, newDAG.edges());
+		assertEquals("", 10, newDAG.vertices());
+		String ans = "[5, 6]";
+		assertEquals("",ans, newDAG.adj(4).toString());
+	}
 }
